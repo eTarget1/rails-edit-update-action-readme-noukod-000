f9a80fd099ad6f17423033503c7ebb408261a740
clifford <clifford@noukod.com>
2019-01-28 19:36:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,10 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+	
 	def edit
-  @article = Article.find(params[:id])
-end
+	  @post = Post.find(params[:id])
+	end
 
 	def update
   @post = Post.find(params[:id])
